c9984d5c76e63a8fe73544981d2f1daad202e084
Recheescript. <rechiejames.postanes@bisu.edu.ph>
2026-04-02 22:35:58 +0800

    sync

--- bisu-api-new/config/cors.php
+++ bisu-api-new/config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://bisu-mst.vercel.app'],
+    'allowed_origins' => ['https://bisu-mst.vercel.app', 'http://localhost:5173', 'http://127.0.0.1:5173'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
+    'max_age' => 86400,
+    'supports_credentials' => false,
 ];
